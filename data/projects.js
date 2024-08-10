1787ff99991687a7f67a7079d393de1c500694eb
Helder M. V. R. <manuhelderruiz@gmail.com>
2024-08-10 12:22:06 +0200

    update

--- data/projects.js
+++ data/projects.js
@@ -3,7 +3,7 @@ export const projects = [
     title: "Skyrimzon",
     description: "Tienda web que simula la venta de objetos del mundo de Skyrim. Ofrece una variedad de artículos icónicos, como armas, armaduras y pociones, recreados para los fans del juego.",
     image:
-      "assets/project_skyrimzon.png",
+      "./assets/project_skyrimzon.png",
     tech: ["HTML5", "CSS3", "JavaScript"],
     github: "https://github.com/theHELDERscrolls/practica_DOM",
     link: "https://skyrimzon.vercel.app/",
@@ -12,7 +12,7 @@ export const projects = [
     title: "The Raven Age - Unofficial",
     description: "Simulación de la web del grupo The Raven Age.",
     image:
-      "assets/project_tra.png",
+      "./assets/project_tra.png",
     tech: ["HTML5", "CSS3"],
     github: "https://github.com/theHELDERscrolls/01_Project",
     link: "https://theravenageunofficial.vercel.app/",
