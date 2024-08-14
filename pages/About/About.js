827db57a62c44b53fd55bdfa63fcfe3b29c4f6e3
Helder M. V. R. <manuhelderruiz@gmail.com>
2024-08-14 12:55:00 +0200

    Update: smooth transition

--- pages/About/About.js
+++ pages/About/About.js
@@ -5,9 +5,9 @@ export const About = () => {
   const main = document.querySelector("main");
   cleanPage(main);
   main.innerHTML = `
-    <div id="construction">
+    <section id="construction">
       <iframe src="https://giphy.com/embed/hjLtnBNLduUyA">
       </iframe>
-    </div>
+    </section>
 `;
 };
